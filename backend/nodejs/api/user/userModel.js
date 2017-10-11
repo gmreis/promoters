3f3d54b24aa47fa7ae39e5d99daa83c141e5e54c
Geraldo Mateus Reis <reis.geraldomateus@gmail.com>
2017-10-10 23:52:37 -0300

    Corrigido Login

--- backend/nodejs/api/user/userModel.js
+++ backend/nodejs/api/user/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  faceId: { type: Number, required: true, index: true},
+  faceId: { type: Number, required: true, index: {unique: true} },
   name: { type: String, required: true},
   sexo: { type: String, required: true},
   photo: { type: String, required: true},
-  birth: { type: String, required: true},
-  position: { type: Number, required: true },
-  level: { type: Number, required: true }
+  birth: { type: String},
+  position: { type: Number },
+  level: { type: Number }
 }, {timestamps: true });
 
 const User = mongoose.model('User', userSchema);
--- backend/nodejs/api/user/userService.js
+++ backend/nodejs/api/user/userService.js
@@ -3,29 +3,44 @@ const User = require('./userModel');
 // POST /api/user
 function login(req, res){
 
-    // TODO: birth
-    var user = new User({
-        faceId: req.body.faceId,
-        name: req.body.name,
-        sexo: req.body.sexo,
-        photo: req.body.photo,
-        birth: req.body.birth,
-        position: req.body.position,
-        level: req.body.level
-    });
 
-    user.save().then(() => {
-        res.status(200).end(JSON.stringify({user}));
-    }).catch(err => {
-        console.error('save', err);
+    // TODO: Alterar para faceId como ID do usuario?
+    if(req.body.hasOwnProperty('faceId')) {
+        
+        User.findOne({faceId: req.body.faceId}) .exec()
+        .then(user => {
 
-        var errors = {};
-        for (var error in err.errors) {
-            errors[error] = (err.errors[error]['message']);
-        };
+            if(!user) {
+                console.log('Antes: ', user);
+                user = new User({
+                    faceId: req.body.faceId,
+                    name: req.body.name,
+                    sexo: req.body.sexo,
+                    photo: req.body.photo,
+                });
+                console.log('Depois', user);
+                
+                return User.create(user);
+            }
 
+            res.status(200).end(JSON.stringify({user}));
+
+        }).then(user => {
+            res.status(200).end(JSON.stringify({user}));
+        })
+        .catch(err => {
+            console.log(err);
+            var errors = {};
+            for (var error in err.errors) {
+                errors[error] = (err.errors[error]['message']);
+            };
+    
+            res.status(400).end(JSON.stringify({errors}));
+        })
+        
+    } else {
         res.status(400).end(JSON.stringify({errors}));
-    })
+    }
 
 }
 
